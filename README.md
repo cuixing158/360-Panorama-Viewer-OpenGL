4a9c5755762c78793b463765e9ab7f68395a740a
cuixing158 <1229261804@qq.com>
2024-12-03 14:00:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# PanoViewer #
+
+a small panoramic image viewer for equirectangular projections written
+in C++ using OpenGL. Projection is done directly in the fragment shader
+on the GPU, and it uses a tiling method to be able to load large images
+that exceed the maximum size of a texture on the graphics card.
+
+## Notes
+
+崔星星记录
+
+## Dependencies ##
+
+PanoViewer depends on GLEW <http://glew.sourceforge.net/> and GLFW 3 <http://www.glfw.org/> for platform independent OpenGL and libjpeg <http://www.ijg.org/> for loading jpg's.
+
+Libjpeg is statically included in the code, just put the content of
+jpegsr<x>.zip from the IJG in the "libjpeg" subdirectory before running
+CMake
+
+## Usage ##
+
+drag and rop any .jpg file on the panoviewer window to load the image
+
+mouse:
+
+- hold LMB and move : drag screen
+- hold RMB and move : scroll in direction
+
+keyboard:
+
+- H to toggle on screen help text
+- W,S to rotate view up/down
+- Q,E to de/increase field of view
+- C toggle compatibility render mode (shaders on/off)
+- SPACE toggle on-screen text
---
+++ cmake/FindGLEWW.cmake
@@ -0,0 +1,67 @@
+#
+# Try to find GLEW library and include path.
+# Once done this will define
+#
+# GLEW_FOUND
+# GLEW_INCLUDE_PATH
+# GLEW_LIBRARY
+# 
+
+set(GLEW_ROOT_DIR "E:/softwares/glew-2.1.0-win32/glew-2.1.0")
+IF (WIN32)
+# static
+	# FIND_PATH( GLEW_INCLUDE_PATH 
+	# 	DOC "The directory where GL/glew.h resides")
+	# FIND_FILE( GLEW_SRC_FILE glew.c
+	#     PATHS ${GLEW_INCLUDE_PATH}/../src/
+	# 	)
+	set(GLEW_INCLUDE_PATH ${GLEW_ROOT_DIR}/include)
+	find_library(GLEW_LIBRARY NAMES glew GLEW glew32 glew32s
+	PATHS
+	${GLEW_ROOT_DIR}/lib/Release/x64
+		DOC "The GLEW library")
+	message(STATUS, "GLEW_LIBRARY库文件: ${GLEW_LIBRARY},头文件GLEW_INCLUDE_PATH：${GLEW_INCLUDE_PATH}")
+	ADD_DEFINITIONS(-DGLEW_STATIC)
+	
+	#FIND_LIBRARY( GLEW_LIBRARY
+#		NAMES glew GLEW glew32 glew32s
+#		PATHS
+#		$ENV{PROGRAMFILES}/GLEW/lib
+#		${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
+#		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
+#		DOC "The GLEW library")
+ELSE (WIN32)
+    SET(GLEW_SRC_FILE,"")
+	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+		PATHS /usr/include
+		/usr/local/include
+		/sw/include
+		/opt/local/include
+		DOC "The directory where GL/glew.h resides")
+        #message("GLEW include: ${GLEW_INCLUDE_PATH}")
+	FIND_LIBRARY( GLEW_LIBRARY
+		NAMES GLEW glew
+		PATHS
+		/usr/lib64
+		/usr/lib
+		/usr/local/lib64
+		/usr/local/lib
+		/sw/lib
+		/opt/local/lib
+		DOC "The GLEW library")
+	#message("GLEW lib: ${GLEW_LIBRARY}")
+ENDIF (WIN32)
+
+IF (DEFINED GLEW_INCLUDE_PATH)
+	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+ELSE (DEFINED GLEW_INCLUDE_PATH)
+	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+ENDIF (DEFINED GLEW_INCLUDE_PATH)
+
+if(GLEW_FOUND)
+  message(STATUS "GLEW found!")
+else(GLEW_FOUND)
+  message(STATUS "GLEW NOT found!")
+endif(GLEW_FOUND)
+
+MARK_AS_ADVANCED( GLEW_FOUND )
